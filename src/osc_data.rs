40083e9f11528112e5670d0011ae9a1155ca14a7
Michael Gehring <mg@ebfe.org>
2014-09-23 20:04:54 +0200

    Vec::push_all_move is deprecated

--- src/osc_data.rs
+++ src/osc_data.rs
@@ -114,7 +114,7 @@ pub fn get_args_with_addr(packet: OscPacket, addr_match: &str) -> Option<Vec<Osc
 			let mut arg_vec = Vec::new();
 			for subpacket in conts.into_iter() {
 				match get_args_with_addr(subpacket, addr_match) {
-					Some(a) => arg_vec.push_all_move(a),
+					Some(a) => arg_vec.extend(a.into_iter()),
 					None => ()
 				}
 			}
